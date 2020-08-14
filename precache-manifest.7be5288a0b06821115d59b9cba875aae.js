6b433beb9f185b082b9e581f591b4009f37f0b61
vikadonika <donika.vika@gmail.com>
2020-08-14 18:22:32 +0300

    Updates

---
+++ precache-manifest.7be5288a0b06821115d59b9cba875aae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8bcdc511b6b3c22e7dfd1f95cca3998",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "eaa5d9b5f81bef446c0e",
+    "url": "/weather/static/css/main.7d9b8ecc.chunk.css"
+  },
+  {
+    "revision": "62928de0c833575d51d9",
+    "url": "/weather/static/js/2.92a96aae.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/weather/static/js/2.92a96aae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaa5d9b5f81bef446c0e",
+    "url": "/weather/static/js/main.029bbcf6.chunk.js"
+  },
+  {
+    "revision": "002da698503cdf7f02a6",
+    "url": "/weather/static/js/runtime-main.962d4151.js"
+  }
+]);
